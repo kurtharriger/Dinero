68a52f6cb0c08259ba90c464a77e8155369c698d
Charles Ferentchak <charles@rallydev.com>
2014-12-27 11:17:55 -0700

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var comments =[
+  {
+    "author": "Pete Hunt",
+    "text": "Hey there!"
+  }
+];
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/comments.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(comments));
+});
+
+app.post('/comments.json', function(req, res) {
+  comments.push(req.body);
+  console.log(comments);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(comments));
+});
+
+app.listen(2001);
+
+console.log('Server started: http://localhost:2001/');
