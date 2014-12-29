ec6c6da60774cda6203f40652e94f24bcf776fec
Charles Ferentchak <charles@rallydev.com>
2014-12-29 15:33:38 -0700

    first nested component

--- dist/js/todolist.jsx
+++ dist/js/todolist.jsx
@@ -1,10 +1,19 @@
 
-
+var DateDisplay = React.createClass({
+  render:function(){
+    return ( 
+    <div className="dateDisplay">
+      {this.props.date}
+    </div>
+    );
+  }
+});
 var Item = React.createClass({
   getInitialState: function() {
     return {        
       owner:"/img/ownerImage.jpg",
-      text:"default text"
+      text:"default text",
+      date:"1/2/2015"
     };
   },
   render: function() {
@@ -13,9 +22,10 @@ var Item = React.createClass({
         <div className="gripper"></div>
         <label className="checkbox-label">
           <input type="checkbox"/>
+          {this.state.text}
         </label>
-        <img className="avatar" src={this.state.owner}>
-        </img>
+        <img className="avatar" src={this.state.owner}/>
+        <DateDisplay date={this.state.date}/>
       </div>
     );
   }
