0201b6ad7b7474eed4e5e84326113961e001cb48
Charles Ferentchak <charles@rallydev.com>
2014-12-29 18:22:15 -0700

    one of each list item

--- dist/js/todolist.jsx
+++ dist/js/todolist.jsx
@@ -1,31 +1,41 @@
-
 var DateDisplay = React.createClass({
   render:function(){
-    return ( 
-    <div className="dateDisplay">
-      {this.props.date}
-    </div>
-    );
+    if(this.props.date){
+      return ( 
+      <div className="dateDisplay">
+        {this.props.date}
+      </div>
+      )
+    }
+    else{   
+      return ( 
+        <div className="noDateDisplay">
+        </div>
+      )
+    }
   }
 });
+var defaultProps ={
+    text: "",
+    avatarUrl: "/img/noavatar.png",
+    date:null
+};
 var Item = React.createClass({
   getInitialState: function() {
-    return {        
-      owner:"/img/ownerImage.jpg",
-      text:"default text",
-      date:"1/2/2015"
-    };
+    return {};
   },
+
   render: function() {
+    var data = _.defaults(this.props.data,defaultProps);
     return (
       <div className="item">
         <div className="gripper"></div>
         <label className="checkbox-label">
           <input type="checkbox"/>
-          {this.state.text}
+          {this.props.data.text}
         </label>
-        <img className="avatar" src={this.state.owner}/>
-        <DateDisplay date={this.state.date}/>
+        <img className="avatar" src={this.props.data.avatarUrl}/>
+        <DateDisplay date={this.props.data.date}/>
       </div>
     );
   }
--- dist/js/ux-playground.jsx
+++ dist/js/ux-playground.jsx
@@ -1,5 +1,27 @@
-React.render(
-  <Item  />,
-  document.getElementById('item')
-);
 
+var avatarAndDate = {        
+  avatarUrl:"/img/ownerImage.jpg",
+  text:"default text",
+  date:"1/2/2015"
+};
+var avatarAndNoDate = {        
+  avatarUrl:"/img/ownerImage.jpg",
+  text:"default text"
+};
+
+var noAvatarAndNoDate = {        
+  text:"default text"
+};
+
+React.render(
+  <Item data={avatarAndDate} />,
+  document.getElementById('avatar-nodate-item')
+);
+React.render(
+  <Item data={avatarAndNoDate} />,
+  document.getElementById('avatar-date-item')
+);
+React.render(
+  <Item data={noAvatarAndNoDate} />,
+  document.getElementById('noavatar-nodate-item')
+);
